44fea5ffcdbcc40fd220030f2219b20d14c89d2e
jyung17 <iotaxixi@gmail.com>
2022-12-27 01:23:01 +0900

    스프링 MVC(기본기능): HTTP 요청 파라미터 - @ModelAttribute

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,11 +1,13 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import java.io.IOException;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -37,7 +39,7 @@ public class RequestParamController {
   public String requestParamV2(
       @RequestParam("username") String memberName,
       @RequestParam("age") int memberAge) {
-  
+    
     log.info("username={}, age={}", memberName, memberAge);
     return "ok";
   }
@@ -84,11 +86,9 @@ public class RequestParamController {
   
   
   /**
-   * @RequestParam
-   * - defaultValue 사용
-   *
-   * 참고: defaultValue는 빈 문자의 경우에도 적용
-   * /request-param?username=
+   * @RequestParam - defaultValue 사용
+   * <p>
+   * 참고: defaultValue는 빈 문자의 경우에도 적용 /request-param?username=
    */
   @ResponseBody
   @RequestMapping("/request-param-default")
@@ -101,9 +101,8 @@ public class RequestParamController {
   }
   
   /**
-   * @RequestParam Map, MultiValueMap
-   * Map(key=value)
-   * MultiValueMap(key=[value1, value2, ...]) ex) (key=userIds, value=[id1, id2])
+   * @RequestParam Map, MultiValueMap Map(key=value) MultiValueMap(key=[value1, value2, ...]) ex)
+   * (key=userIds, value=[id1, id2])
    */
   @ResponseBody
   @RequestMapping("/request-param-map")
@@ -112,4 +111,28 @@ public class RequestParamController {
     return "ok";
   }
   
+  /**
+   * @ModelAttribute 사용
+   * 참고: model.addAttribute(helloData) 코드도 함께 자동 적용됨, 뒤에 model을 설명할 때 자세히 설명
+   */
+  @ResponseBody
+  @RequestMapping("/model-attribute-v1")
+  public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+    log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+    log.info("helloData={}", helloData);
+    return "ok";
+  }
+  
+  /**
+   * @ModelAttribute 생략 가능
+   * String, int 같은 단순 타입 = @RequestParam
+   * argument resolver 로 지정해둔 타입 외 = @ModelAttribute
+   */
+  @ResponseBody
+  @RequestMapping("/model-attribute-v2")
+  public String modelAttributeV2(HelloData helloData) {
+    log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+    log.info("helloData={}", helloData);
+    return "ok";
+  }
 }
