8eebd473d55c305a426418672d4b54c944f0539a
jyung17 <iotaxixi@gmail.com>
2022-12-29 02:38:27 +0900

    스프링 MVC(웹 페이지 만들기): 상품 상세

--- item-service/src/main/java/hello/itemservice/web/basic/ItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/ItemController.java
@@ -5,10 +5,10 @@ import hello.itemservice.domain.item.ItemRepository;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -25,6 +25,13 @@ public class ItemController {
     return "basic/items";
   }
   
+  @GetMapping("/{itemId}")
+  public String item(@PathVariable long itemId, Model model) {
+    Item item = itemRepository.findById(itemId);
+    model.addAttribute("item", item);
+    return "basic/item";
+  }
+  
   /**
    * 테스트용 데이터 추가
    */
@@ -32,6 +39,5 @@ public class ItemController {
   public void init() {
     itemRepository.save(new Item("itemA", 10000, 10));
     itemRepository.save(new Item("itemB", 20000, 20));
-    
   }
 }
