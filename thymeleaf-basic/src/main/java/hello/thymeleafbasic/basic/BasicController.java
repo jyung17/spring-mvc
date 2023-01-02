5d4a64f32c721dd1f8b8a3898bc3b8822d9c04ac
jyung17 <iotaxixi@gmail.com>
2023-01-02 21:04:51 +0900

    타임리프(기본 기능): 조건부 평가

--- thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -99,13 +99,19 @@ public class BasicController {
     return "basic/each";
   }
   
+  @GetMapping("/condition")
+  public String condition(Model model) {
+    addUsers(model);
+    return "basic/condition";
+  }
+  
   private void addUsers(Model model) {
     List<User> list = new ArrayList<>();
     list.add(new User("userA", 10));
     list.add(new User("userB", 20));
     list.add(new User("userC", 30));
     list.add(new User("userD", 40));
-  
+    
     model.addAttribute("users", list);
   }
   
