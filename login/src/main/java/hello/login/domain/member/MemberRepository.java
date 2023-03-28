d76b1e71dcb9e71e7f321fe50c981d7897251854
jyung17 <iotaxixi@gmail.com>
2023-03-28 23:17:17 +0900

    로그인 처리1-쿠키, 세션 로그인 요구사항, 프로젝트 생성, 홈 화면, 회원 가입

---
+++ login/src/main/java/hello/login/domain/member/MemberRepository.java
@@ -0,0 +1,66 @@
+package hello.login.domain.member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Slf4j
+@Repository
+public class MemberRepository {
+  
+  private static Map<Long, Member> store = new HashMap<>(); //static 사용
+  private static long sequence = 0L; //static 사용
+  
+  public Member save(Member member) {
+    member.setId(++sequence);
+    log.info("save: member={}", member);
+    store.put(member.getId(), member);
+    return member;
+  }
+  
+  public Member findById(Long id) {
+    return store.get(id);
+  }
+  
+  // lambda 사용전
+  /*  public Member findByLoginId(String loginId) {
+      List<Member> all = findAll();
+      for (Member m : all) {
+        if (m.getLoginId().equals(loginId)) {
+          return m;
+        }
+      }
+      return null;
+    }*/
+  
+  // Optional 사용
+  /*public Optional<Member> findByLoginId(String loginId) {
+    List<Member> all = findAll();
+    for (Member m : all) {
+      if (m.getLoginId().equals(loginId)) {
+        return Optional.of(m);
+      }
+    }
+    return Optional.empty();
+  }*/
+  
+  // Optional lambda 사용
+  public Optional<Member> findByLoginId(String loginId) {
+    return findAll().stream()
+        .filter(m -> m.getLoginId().equals(loginId))
+        .findFirst();
+  }
+  
+  public List<Member> findAll() {
+    return new ArrayList<>(store.values());
+  }
+  
+  public void clearStore() {
+    store.clear();
+  }
+}
